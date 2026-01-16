c3d64f09c4aaf8cb82793c82874175afb6141ffe
Farel Rizky <147903975+FarelX@users.noreply.github.com>
2026-01-17 00:00:43 +0700

    New Update

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
-let inputEl = document.getElementById("input-el");
-let convertBtn = document.getElementById("convert-el");
-let lengthEl = document.getElementById("length-el");
-let volumeEl = document.getElementById("volume-el");
-let massEl = document.getElementById("mass-el");
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-el");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const massEl = document.getElementById("mass-el");
 
-const length = {
-  meterToFeet: 3.281,
+const convert = {
+  meterToFeet: 3.28084,
   feetToMeter: 0.3048,
-};
-const volume = {
-  literToGallon: 0.264,
-  gallonToLiter: 3.785,
-};
-const mass = {
-  kiloToPound: 2.204,
-  poundToKilo: 0.453,
+  literToGallon: 0.21997,
+  gallonToLiter: 4.54609,
+  kiloToPound: 2.20462,
+  poundToKilo: 0.453592,
 };
 
 convertBtn.addEventListener("click", function () {
-  let baseValue = inputEl.value;
-  let resultMeter = baseValue * length.meterToFeet;
-  let resultFeet = baseValue * length.feetToMeter;
-  let resultLiter = baseValue * volume.literToGallon;
-  let resultGallon = baseValue * volume.gallonToLiter;
-  let resultKilo = baseValue * mass.kiloToPound;
-  let resultPound = baseValue * mass.poundToKilo;
-
-  // (baseValue * length.feetToMeter);
-  lengthEl.innerHTML = `
-  ${baseValue} meter = ${resultMeter.toFixed(
-    3
-  )} feet | ${baseValue} feet = ${resultFeet.toFixed(3)} meter  `;
-
-  volumeEl.innerHTML = `
-  ${baseValue} liters = ${resultLiter.toFixed(
-    3
-  )} gallons | ${baseValue} gallons = ${resultGallon.toFixed(3)} liters
-`;
-
-  massEl.innerHTML = `
-  ${baseValue} kilograms = ${resultKilo.toFixed(
-    3
-  )} pounds | ${baseValue} pounds = ${resultPound.toFixed(3)} kilograms
-`;
+  let baseValue = Number(inputEl.value);
+  lengthEl.textContent = `${baseValue} meters = ${(
+    baseValue * convert.meterToFeet
+  ).toFixed(3)} feet | ${baseValue} feet = ${(
+    baseValue * convert.feetToMeter
+  ).toFixed(3)} meters`;
+  volumeEl.textContent = `${baseValue} liters = ${(
+    baseValue * convert.literToGallon
+  ).toFixed(3)} gallons | ${baseValue} gallons = ${(
+    baseValue * convert.gallonToLiter
+  ).toFixed(3)} liters`;
+  massEl.textContent = `${baseValue} kilograms = ${(
+    baseValue * convert.kiloToPound
+  ).toFixed(3)} pounds | ${baseValue} pounds = ${(
+    baseValue * convert.poundToKilo
+  ).toFixed(3)} kilograms`;
 });
